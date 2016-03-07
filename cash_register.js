4c8af6f4da8b66e7eb9b90f21638b8cc15898ef7
philiptang42 <philiptang42@gmail.com>
2016-03-06 23:07:45 -0500

    using add method

--- cash_register.js
+++ cash_register.js
@@ -6,3 +6,20 @@ var cashRegister = {
 
 //Using dot notation change the total property
 cashRegister.total = 2.99
+-------------------------------
+var cashRegister = {
+    total:0,
+    add: function(itemCost){
+        this.total += itemCost;
+    }
+};
+
+
+//call the add method for our items
+cashRegister.add(0.98)
+cashRegister.add(1.23)
+cashRegister.add(4.99)
+cashRegister.add(0.45)
+
+//Show the total bill
+console.log('Your bill is '+cashRegister.total);
