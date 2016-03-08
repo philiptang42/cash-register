da688b2707e590ea4968992ee00a7aaba632260b
philiptang42 <philiptang42@gmail.com>
2016-03-08 07:07:10 -0500

    applying staff member discount

--- cash_register.js
+++ cash_register.js
@@ -135,3 +135,15 @@ var bob = new StaffMember("Bob",10);
 
 //Create a StaffMember for yourself called me
 var me = new StaffMember("Phil", 20);
+-------------------------------
+// create a constructor for the StaffMember class
+var StaffMember = new Object(); {
+  name = name;
+  discountPercent = discountPercent;
+};
+
+var sally = new StaffMember("Sally",5);
+var bob = new StaffMember("Bob",10);
+
+//Create a StaffMember for yourself called me
+var me = new StaffMember("Phil", 20);
